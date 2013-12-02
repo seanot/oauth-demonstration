0fd95ec4fd028ee020a78c668a797d2a74d2f21d
seanot <sean@kingfieldcapital.com>
2013-12-01 22:18:18 -0600

    moved authentication code to Authentication service

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,66 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if auth = env['omniauth.auth']
-      @user = User.where(auth.slice(:info['email'])).first_or_initialize.tap do |user|
-        puts "==========#{user.name} #{user.email}"
-        user.name = auth.info.name
-        user.email = auth.info.email if user.email == nil
-        user.save!
-        identity = Identity.find_by_provider_and_uid(auth['provider'], auth['uid'].to_s)
-        if identity.nil?
-          identity = Identity.create(uid: auth['uid'], provider: auth['provider'], user_id: user.id)
-        end
-      end
-      session[:user_id] = @user.id
+    auth = Authentication.new(params, env['omniauth.auth'])
+    if auth.authenticated?
+      session[:user_id] = auth.user.id
       flash[:notice] = "Logged in."
       redirect_to :root
     else
-      user = User.find_by(email: params[:email])
-      @user = user if user && user.authenticate(params[:email], params[:password])
-      session[:user_id] = @user.id
-      flash[:notice] = "Logged in."
-      redirect_to :root
+      flash[:notice] = "Email or password invalid."
+      render :new
     end
   end
-
-
-  #     if signed_in?
-  #       if @identity.user == current_user
-  #         flash[:notice] = "Already linked to that account."
-  #         redirect_to :root
-  #       else
-  #         @identity.user = current_user
-  #         @identity.save
-  #         flash[:notice] = "Successfully linked to that account."
-  #         redirect_to :root
-  #       end
-  #     else
-  #       if @identity.user.present?
-  #         self.current_user = @identity.user
-  #         flash[:notice] = "Signed in."
-  #         redirect_to :root
-  #       else
-  #         current_user = @identity.user
-  #         flash[:notice] = "Thank you for registering."
-  #         redirect_to :root
-  #       end
-  #     end
-  #   else
-  #     #
-  #       session[:user_id] = user.id
-  #
-  #     else
-  #       flash[:notice] = "Please re-enter your credentials."
-  #       redirect_to new_user_path
-  #     end
-  #   end
-  # end
 
   def destroy
     self.current_user = nil
     flash[:notice] = "Signed out."
     redirect_to :root
   end
-
 end
--- app/services/authentication.rb
+++ app/services/authentication.rb
@@ -1,3 +1,38 @@
 class Authentication
+  def initialize(params, omniauth = nil)
+    @params = params
+    @omniauth = omniauth
+  end
+
+  def user
+    @user ||= @omniauth ? user_from_omniauth : user_with_password
+  end
+
+  def authenticated?
+    user.present?
+  end
+
+  private
+    def user_from_omniauth
+      User.where(@omniauth.slice(:info['email'])).first_or_initialize.tap do |user|
+        puts "==========#{user.name} #{user.email}"
+        user.name = @omniauth.info.name
+        user.email = @omniauth.info.email if user.email == nil
+        user.save!
+        identity = Identity.find_by_provider_and_uid(
+                  @omniauth['provider'],
+                  @omniauth['uid'].to_s)
+        if identity.nil?
+          identity = Identity.create(uid: @omniauth['uid'],
+                                provider: @omniauth['provider'],
+                                 user_id: user.id)
+        end
+      end
+    end
+
+    def user_with_password
+      user = User.find_by(email: params[:email])
+      user && user.authenticate(params[:email], params[:password])
+    end
 
 end
